6d3d393738ca9a02105417d9a6c94f1e61155ef5
Gerardo Garcia Herrera <gerardo.garcia@broxel.com>
2018-01-17 22:32:43 -0600

    Updated environment heroku URL

--- config/environments.js
+++ config/environments.js
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   development: {
-    API_URL: 'https://thawing-springs-94357.herokuapp.com',
+    API_URL: 'https://glacial-plateau-68528.herokuapp.com',
     API_PORT: '5000'
   },
 
