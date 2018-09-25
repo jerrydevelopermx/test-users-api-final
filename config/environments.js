e7b688d949c514923adaebc3760308bd758dc23e
Gerardo Garcia <jerry@Gerardos-MacBook-Pro.local>
2018-09-25 08:45:14 -0500

    Added errors logging api

--- config/environments.js
+++ config/environments.js
@@ -12,17 +12,17 @@ module.exports = {
   },
 
   development: {
-    API_URL: 'https://glacial-plateau-68528.herokuapp.com',
+    API_URL: 'https://sheltered-tor-73864.herokuapp.com',
     API_PORT: '5000'
   },
 
   staging: {
-    API_URL: 'https://glacial-plateau-68528.herokuapp.com',
+    API_URL: 'https://sheltered-tor-73864.herokuapp.com',
     API_PORT: '5000'
   },
 
   production: {
-    API_URL: 'https://glacial-plateau-68528.herokuapp.com',
+    API_URL: 'https://sheltered-tor-73864.herokuapp.com',
     API_PORT: '5000'
   }
 
